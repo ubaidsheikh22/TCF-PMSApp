67b53b6803ab28166a747c535f7774c00eb7240f
hp <muhammadzakir2000@hotmail.com>
2019-10-04 06:59:09 -0700

    android project pms

---
+++ app/src/main/java/com/example/myapplication/selectRegionActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class selectRegionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_region);
+
+        Spinner spinner1=(Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> myadapter =new ArrayAdapter<String>(selectRegionActivity.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names));
+            myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner1.setAdapter(myadapter   );
+
+    }
+}
